5d8e86ed2c9385f6b1196019758365963f2f454f
cj <cj@authing.cn>
2020-11-12 13:15:42 +0800

    feat: add loginByCode loginByPhone and getPhone method

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,56 @@ export class AuthenticationClient extends BaseAuthenticationClient {
     options.graphqlClient = GraphqlClient;
     super(options);
   }
+
+  /**
+   * @description 通过 wx.login 获取到的 code 登录
+   *
+   */
+  async loginByCode(
+    code: string,
+    options: {
+      iv?: string;
+      encryptedData?: string;
+      phone?: string;
+      // wx.getUserInfo 返回的 rawData, 里面包含了原始用户数据
+      rawData?: string;
+    }
+  ) {
+    const api = `${this.options.host}/connections/social/wechat-miniprogram/auth`;
+    const { iv, encryptedData, phone, rawData } = options;
+    const data = await this.httpClient.request({
+      method: "POST",
+      url: api,
+      data: { code, iv, encryptedData, phone, rawData },
+    });
+    return data;
+  }
+
+  /**
+   * @description 通过获取手机号开放组件事件登录
+   *
+   */
+  async loginByPhone(code: string, iv: string, encryptedData: string) {
+    const api = `${this.options.host}/connections/social/wechat-miniprogram/auth-by-phone`;
+    const data = await this.httpClient.request({
+      method: "POST",
+      url: api,
+      data: { code, iv, encryptedData },
+    });
+    return data;
+  }
+
+  /**
+   * @description 获取用户的手机号
+   *
+   */
+  async getPhone(code: string, iv: string, encryptedData: string) {
+    const api = `${this.options.host}/connections/social/wechat-miniprogram/getphone`;
+    const data = await this.httpClient.request({
+      method: "POST",
+      url: api,
+      data: { code, iv, encryptedData },
+    });
+    return data;
+  }
 }
